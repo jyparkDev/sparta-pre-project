6d266ef7c242f5b562badf298ff195281b66510c
jy.park <pjjy92@gmail.com>
2022-12-25 23:40:55 +0900

    git init
    spring으로 구현하기 위한 프로젝트 생성

---
+++ src/main/java/sparta/prestudy/PreStudyApplication.java
@@ -0,0 +1,13 @@
+package sparta.prestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sparta/prestudy/PreStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.prestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
